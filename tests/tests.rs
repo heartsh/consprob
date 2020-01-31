f308c94e74f3caa9a8fd936082b7ad6b3ab46249
heartsh <heartsh@heartsh.io>
2020-01-31 10:08:31 +0900

    Fix test

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,15 +1,15 @@
-extern crate zprob;
+extern crate phyloprob;
 extern crate time;
 
 use time::precise_time_s;
 use std::process::{Command, Output};
 
 #[test]
-fn test_zprob_program() {
+fn test_phyloprob_program() {
   let input_fasta_file_path = "assets/sampled_trnas.fa";
   let args = ["-i", input_fasta_file_path, "-o", "assets/sampled_trnas"];
   let begin = precise_time_s();
-  run_command("target/release/zprob", &args, "Failed to run the Zprob program.");
+  run_command("target/release/phyloprob", &args, "Failed to run the PhyloProb program.");
   let elapsed_time = precise_time_s() - begin;
   println!("The elapsed time for computing the posterior probabilities on structural alignment given each pair of RNA sequences in the FASTA file \"{}\" = {} [s].", input_fasta_file_path, elapsed_time);
 }
